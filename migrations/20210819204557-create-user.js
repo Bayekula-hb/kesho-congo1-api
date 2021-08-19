33036f92c050b962f5d703f437b35e556e4082a2
Bayekula-hb <hobedbayekula@gmail.com>
2021-08-19 22:55:35 +0100

    feat create the cause_malnutrition model

--- migrations/20210819204557-create-user.js
+++ migrations/20210819204557-create-user.js
@@ -1,42 +1,43 @@
-'use strict';
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
+    await queryInterface.createTable("users", {
       id_user: {
-        type: Sequelize.STRING
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING,
       },
       nom_user: {
-        type: Sequelize.STRING
+        allowNull: false,
+        type: Sequelize.STRING,
       },
       postnom_user: {
-        type: Sequelize.STRING
+        allowNull: false,
+        type: Sequelize.STRING,
       },
       prenom_user: {
-        type: Sequelize.STRING
+        allowNull: false,
+        type: Sequelize.STRING,
       },
       email: {
-        type: Sequelize.STRING
+        allowNull: false,
+        type: Sequelize.STRING,
       },
       password: {
-        type: Sequelize.STRING
+        allowNull: false,
+        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('users');
-  }
+    await queryInterface.dropTable("users");
+  },
 };
---
+++ migrations/20210819215433-create-cause-malnutrition.js
@@ -0,0 +1,92 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('cause_malnutritions', {
+      // id: {
+      //   allowNull: false,
+      //   autoIncrement: true,
+      //   primaryKey: true,
+      //   type: Sequelize.INTEGER
+      // },
+      id_cause_malnutrition: {
+        allowNull: false,
+        type: Sequelize.STRING
+        primaryKey: true,
+      },
+      atcd_mas: {
+        type: Sequelize.BOOLEAN
+      },
+      nbre_chute: {
+        type: Sequelize.NUMBER
+      },
+      mas_fratrie: {
+        type: Sequelize.BOOLEAN
+      },
+      terme_grossesse: {
+        type: Sequelize.STRING
+      },
+      sejour_neonat: {
+        type: Sequelize.BOOLEAN
+      },
+      eig: {
+        type: Sequelize.FLOAT
+      },
+      lieu_accouchement: {
+        type: Sequelize.STRING
+      },
+      asphyxie_perinatal: {
+        type: Sequelize.BOOLEAN
+      },
+      dpm: {
+        type: Sequelize.STRING
+      },
+      calendrier_vaccinal: {
+        type: Sequelize.BOOLEAN
+      },
+      rang_fratrie: {
+        type: Sequelize.INTEGER
+      },
+      taille_fratrie: {
+        type: Sequelize.INTEGER
+      },
+      atcd_rougeole_fratrie: {
+        type: Sequelize.BOOLEAN
+      },
+      vaccination_rougeole: {
+        type: Sequelize.BOOLEAN
+      },
+      terrain_vih: {
+        type: Sequelize.BOOLEAN
+      },
+      tbc: {
+        type: Sequelize.BOOLEAN
+      },
+      atcd_du_tbc_dans_fratrie: {
+        type: Sequelize.BOOLEAN
+      },
+      hospitalisation_recente: {
+        type: Sequelize.BOOLEAN
+      },
+      diagnostique_hospitalisation: {
+        type: Sequelize.TEXT
+      },
+      cocktail_atb: {
+        type: Sequelize.BOOLEAN
+      },
+      duree_prise_atb: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('cause_malnutritions');
+  }
+};
