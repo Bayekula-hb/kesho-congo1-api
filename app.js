ee480b89ccfa894cac5889ab24d5cbc6ab1760aa
Bayekula-hb <hobedbayekula@gmail.com>
2021-08-19 21:48:18 +0100

    feat create the user model

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const cors = require("cors");
 const db = require("./utils/Dbconnect");
 const authRoutes = require("./routes/auth.route");
 
-// (async () => {
-//   try {
-//     await db.authenticate();
-//     console.log("Connection has been established successfully.");
-//   } catch (error) {
-//     console.error("Unable to connect to the database:", error);
-//   }
-// })();
+(async () => {
+  try {
+    await db.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+})();
 app.use(express.json());
 app.get("/", (req, res) => {
   res.json({ message: "Votre requête a bien été reçue !" });
---
+++ migrations/20210819204557-create-user.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      id_user: {
+        type: Sequelize.STRING
+      },
+      nom_user: {
+        type: Sequelize.STRING
+      },
+      postnom_user: {
+        type: Sequelize.STRING
+      },
+      prenom_user: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
