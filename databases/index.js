96b59910b76a9d3cb95ddf703fe9c7e32012dfa9
Bayekula-hb <hobedbayekula@gmail.com>
2021-08-24 14:49:16 +0100

    feat after merge with master distant

--- databases/index.js
+++ databases/index.js
@@ -1,6 +1,16 @@
 const { Sequelize } = require("sequelize");
 
 // Configuration de la base de donnée
+// const db = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASS,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: "mysql",
+//   }
+// );
+
 const db = new Sequelize(
     "kesho",
    "root",
