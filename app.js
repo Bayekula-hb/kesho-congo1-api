b274a96f3f8dce11dfdf8b3bd2cc32421360ab55
Bayekula-hb <hobedbayekula@gmail.com>
2021-09-20 16:34:02 +0100

    refactor : add the cors options

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const corsOptions = {
   optionSuccessStatus: 200,
 };
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptions), next());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
