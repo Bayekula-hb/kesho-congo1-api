108cfbeb574962b74a8e6d9dd95310f96770e123
Bayekula-hb <hobedbayekula@gmail.com>
2021-09-20 17:52:33 +0100

    refactor: accept the all origin

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const routeReporting = require("./routes/reporting");
 const passport = require("passport");
 
 const corsOptions = {
-  origin: ["http://localhost:3000", "https://kesho-congo-1-web-l72kijbun-keshocongo1.vercel.app/", "*"],
+  origin: ["http://localhost:3000", "https://kesho-congo-1-web-l72kijbun-keshocongo1.vercel.app/"],
   credentials: true,
   optionSuccessStatus: 200,
 };
