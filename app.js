beb181077376a3f4d880aadb5c3e51e87d0efb42
Bayekula-hb <hobedbayekula@gmail.com>
2021-09-21 08:22:54 +0100

    rfeat: add the options cors with the middleware

--- app.js
+++ app.js
@@ -23,7 +23,13 @@ const corsOptions1 = {
 };
 // app.use(cors(corsOptions));
 // app.use(cors(corsOptions1));
-app.options('*', cors())
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
